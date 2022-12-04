42dc06150b846ea7a76a3aeec96b5b8be21f1881
alexandra.stefan01 <alexandra.stefan01@DESKTOP-DN7QSGB>
2022-12-05 00:37:12 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+	string text;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+	void EntryNumber(object sender, EventArgs e)
+	{
+		text = ((Entry)sender).Text;
+	}
+	void EntryChanged(object sender ,TextChangedEventArgs e)
+	{
+			text = e.NewTextValue;
+
+	}
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		float val = float.Parse(text);
+		float Euro = 4.94F;
+		val= val * Euro;
+		ConverterPlace.Text = $" {val.ToString("0.00")}lei";
+
+		SemanticScreenReader.Announce(ConverterPlace.Text);
+
+	}
+
+
+}
+
+	
+
+ 
